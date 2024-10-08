ea5cafa0dba8d259c552778fa022303952d294e3
Jaap Korthals Altes <jaapkorthalsaltes@gmail.com>
2024-10-08 18:22:28 +0200

    next

--- compile.sh
+++ compile.sh
@@ -10,4 +10,4 @@ $AAPT compile  --dir src/res -o build $VERBOSE
 $AAPT link build/* -o $UNALIGNED   -I /home/jkakeep/Android/Sdk/platforms/android-35/android.jar  --manifest src/AndroidManifest.xml  $VERBOSE
 
 $TOOLPATH/zipalign -f -p 4 $UNALIGNED  $ENDFILE
-$TOOLPATH/apksigner sign --ks-pass 'pass:#Weet6Ik&Veel' --ks /home/jka/Android/androidkey.jks $ENDFILE
+$TOOLPATH/apksigner sign  $ENDFILE
