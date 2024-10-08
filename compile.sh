ef63080109db86e2d8384024cdd4ecffb26e60e6
Jaap Korthals Altes <jaapkorthalsaltes@gmail.com>
2024-10-08 17:32:22 +0200

    First files

---
+++ compile.sh
@@ -0,0 +1,13 @@
+
+TOOLPATH=/home/jka/Android/Sdk/build-tools/35.0.0
+AAPT=$TOOLPATH/aapt2
+VERBOSE=
+UNALIGNED=unaligned.apk
+ENDFILE=signed.apk
+
+$AAPT compile  --dir src/res -o build $VERBOSE
+
+$AAPT link build/* -o $UNALIGNED   -I /home/jkakeep/Android/Sdk/platforms/android-35/android.jar  --manifest src/AndroidManifest.xml  $VERBOSE
+
+$TOOLPATH/zipalign -f -p 4 $UNALIGNED  $ENDFILE
+$TOOLPATH/apksigner sign --ks-pass 'pass:#Weet6Ik&Veel' --ks /home/jka/Android/androidkey.jks $ENDFILE
